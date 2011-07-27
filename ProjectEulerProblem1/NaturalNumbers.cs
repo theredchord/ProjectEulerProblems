05e157587cf2a3818205b31bb67125fb494f44ee
theredchord <botchla411@yahoo.com>
2011-07-26 22:57:37 -0500

    updated files

---
+++ ProjectEulerProblem1/NaturalNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEulerProblem1
+{
+    class NaturalNumbers
+    {
+        //Create list to store numbers
+        List<int> naturalList = new List<int>();
+
+        //Add numbers below 1000 that are multiples of 3 or 5 (natural numbers)
+        public int naturalNumbers()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    naturalList.Add(i);
+                }
+            }
+            return naturalList.Sum();
+        }
+    }
+}
---
+++ ProjectEulerProblem1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEulerProblem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NaturalNumbers numbers = new NaturalNumbers();
+            var finalSum = numbers.naturalNumbers();
+            Console.WriteLine(finalSum);    //Answer: 233168
+        }
+    }
+}
